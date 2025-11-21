a415106b60453df7b296d976b4362230e5ed6a79
PilonQubitserever <serever@pilonqubitventures.com>
2025-11-21 15:42:47 -0600

    Point Services navigation to on-page anchor

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,5 @@
-import ServicesContent from '../components/ServicesContent';
+import { redirect } from 'next/navigation';
 
 export default function ServicesPage() {
-  return <ServicesContent />;
+  redirect('/#services');
 }
