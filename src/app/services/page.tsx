b0080a7269e4da80b98035ac1fdcc1a94aa06654
PilonQubitserever <serever@pilonqubitventures.com>
2025-11-21 18:19:32 -0600

    Restore static export compatibility

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,5 @@
 import ServicesContent from '../components/ServicesContent';
 
-export const revalidate = 0;
-export const dynamic = 'force-dynamic';
-
 export default function ServicesPage() {
   return <ServicesContent />;
 }
