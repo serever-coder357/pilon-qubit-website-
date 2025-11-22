8f26557041adccbd842e65b21355c7439f3663f1
PilonQubitserever <serever@pilonqubitventures.com>
2025-11-21 18:14:11 -0600

    Make services and home pages dynamic

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,8 @@
 import ServicesContent from '../components/ServicesContent';
 
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+
 export default function ServicesPage() {
   return <ServicesContent />;
 }
